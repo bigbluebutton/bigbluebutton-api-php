b1b08428473136c6fa6bcbdafce2286a5117ef23
Ghazi Triki <ghazi.triki@riadvice.tn>
2018-08-12 19:42:48 +0100

    Improve processXmlResponse in BigBlueButton main class.

--- src/BigBlueButton.php
+++ src/BigBlueButton.php
@@ -368,19 +368,19 @@ class BigBlueButton
                 throw new \RuntimeException('Unhandled curl error: ' . curl_error($ch));
             }
             $timeout = 10;
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
             curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-            if ($payload != '') {
+            if (!empty($payload)) {
                 curl_setopt($ch, CURLOPT_HEADER, 0);
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
                 curl_setopt($ch, CURLOPT_POST, 1);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, [
                     'Content-type: ' . $contentType,
-                    'Content-length: ' . strlen($payload),
+                    'Content-length: ' . mb_strlen($payload),
                 ]);
             }
             $data = curl_exec($ch);
@@ -390,18 +390,8 @@ class BigBlueButton
             curl_close($ch);
 
             return new SimpleXMLElement($data);
-        }
-
-        if ($payload != '') {
+        } else {
             throw new \RuntimeException('Post XML data set but curl PHP module is not installed or not enabled.');
-        }
-
-        try {
-            $response = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
-
-            return new SimpleXMLElement($response);
-        } catch (\RuntimeException $e) {
-            throw new \RuntimeException('Failover curl error: ' . $e->getMessage());
         }
     }
 }
