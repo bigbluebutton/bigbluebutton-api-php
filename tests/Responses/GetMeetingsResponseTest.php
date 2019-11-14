af2e6825edb79230e867b526a9d445eff79089f2
Ghazi Triki <ghazi.triki@riadvice.tn>
2019-11-14 16:25:43 +0100

    Fix get_meetings unit tests.

--- tests/Responses/GetMeetingsResponseTest.php
+++ tests/Responses/GetMeetingsResponseTest.php
@@ -62,9 +62,9 @@ class GetMeetingsResponseTest extends TestCase
         $this->assertEquals(2206, $aMeeting->getDuration());
         $this->assertEquals(true, $aMeeting->hasUserJoined());
         $this->assertEquals(14, $aMeeting->getMaxUsers());
-        $this->assertEquals(0, $aMeeting->getModeratorCount());
-        $this->assertEquals('Alessandra Funk', $aMeeting->getMetas()['presenter']);
-        $this->assertEquals('http://www.schiller.info/ut-perspiciatis-et-vero-dolorum-repellendus-nesciunt-commodi-voluptate', $aMeeting->getMetas()['endcallbackurl']);
+        $this->assertEquals(1, $aMeeting->getModeratorCount());
+        $this->assertEquals('Consuelo Gleichner IV', $aMeeting->getMetas()['presenter']);
+        $this->assertEquals('http://www.muller.biz/autem-dolor-aut-nam-doloribus-molestiae', $aMeeting->getMetas()['endcallbackurl']);
     }
 
     public function testGetMeetingsResponseTypes()
