1015e96a66299d95a403d87fe34e4a97a93d9d45
Hannes <31671206+xJuvi@users.noreply.github.com>
2022-09-13 22:09:54 +0200

    fix timeout

--- src/BigBlueButton.php
+++ src/BigBlueButton.php
@@ -484,7 +484,6 @@ class BigBlueButton
             if (!$ch) {
                 throw new \RuntimeException('Unhandled curl error: ' . curl_error($ch));
             }
-            $timeout = 10;
 
             // Needed to store the JSESSIONID
             $cookiefile     = tmpfile();
@@ -498,7 +497,7 @@ class BigBlueButton
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeOut);
             curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiefilepath);
             curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiefilepath);
             if (!empty($payload)) {
