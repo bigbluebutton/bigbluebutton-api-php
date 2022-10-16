a17e208708ccd79915d0f9d92a6b026d822eb7ee
Hannes <31671206+xJuvi@users.noreply.github.com>
2022-10-16 23:10:11 +0200

    Add new features to tests

--- tests/Parameters/CreateMeetingParametersTest.php
+++ tests/Parameters/CreateMeetingParametersTest.php
@@ -91,6 +91,10 @@ class CreateMeetingParametersTest extends TestCase
         $this->assertEquals($params['meta_presenter'], $createMeetingParams->getMeta('presenter'));
         $this->assertEquals($params['meta_endCallbackUrl'], $createMeetingParams->getMeta('endCallbackUrl'));
         $this->assertEquals($params['meta_bbb-recording-ready-url'], $createMeetingParams->getMeta('bbb-recording-ready-url'));
+        
+        $this->assertEquals($params['notifyRecordingIsOn'], $createMeetingParams->getNotifyRecordingIsOn());
+        $this->assertEquals($params['uploadExternalUrl'], $createMeetingParams->getUploadExternalUrl());
+        $this->assertEquals($params['uploadExternalDescription'], $createMeetingParams->getUploadExternalDescription());
 
         // Check values are empty of this is not a breakout room
         $this->assertNull($createMeetingParams->isBreakout());
