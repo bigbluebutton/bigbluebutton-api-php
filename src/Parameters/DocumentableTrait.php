8de307719be533fe05400113a0e420734b46ed29
Ghazi Triki <ghazi.triki@riadvice.tn>
2024-04-22 14:08:48 +0100

    Handle document options for insertDocument API.

--- src/Parameters/DocumentableTrait.php
+++ src/Parameters/DocumentableTrait.php
@@ -20,6 +20,8 @@
 
 namespace BigBlueButton\Parameters;
 
+use BigBlueButton\Parameters\Config\DocumentOptionsStore;
+
 trait DocumentableTrait
 {
     /**
@@ -35,16 +37,13 @@ trait DocumentableTrait
         return $this->presentations;
     }
 
-    /**
-     * @param mixed $content
-     */
-    public function addPresentation(string $nameOrUrl, $content = null, ?string $filename = null): self
+    public function addPresentation(string $nameOrUrl, $content = null, ?string $filename = null, DocumentOptionsStore $attributes = null): self
     {
-        if (!$filename) {
-            $this->presentations[$nameOrUrl] = !$content ?: base64_encode($content);
-        } else {
-            $this->presentations[$nameOrUrl] = $filename;
-        }
+        $this->presentations[$nameOrUrl] = [
+            'content' => !$content ?: base64_encode($content),
+            'filename' => $filename,
+            'attributes' => $attributes
+        ];
 
         return $this;
     }
@@ -52,23 +51,30 @@ trait DocumentableTrait
     public function getPresentationsAsXML(): string
     {
         $result = '';
-
         if (!empty($this->presentations)) {
             $xml    = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><modules/>');
             $module = $xml->addChild('module');
             $module->addAttribute('name', 'presentation');
 
-            foreach ($this->presentations as $nameOrUrl => $content) {
+            foreach ($this->presentations as $nameOrUrl => $data) {
+                $presentation = $module->addChild('document');
                 if (0 === mb_strpos($nameOrUrl, 'http')) {
-                    $presentation = $module->addChild('document');
                     $presentation->addAttribute('url', $nameOrUrl);
-                    if (is_string($content)) {
-                        $presentation->addAttribute('filename', $content);
-                    }
                 } else {
-                    $document = $module->addChild('document');
-                    $document->addAttribute('name', $nameOrUrl);
-                    $document[0] = $content;
+                    $presentation->addAttribute('name', $nameOrUrl);
+                }
+
+                if (!empty($data['filename'])) {
+                    $presentation->addAttribute('filename', $data['filename']);
+                }
+
+                if (!empty($data['content'])) {
+                    $presentation[0] = $data['content'];
+                }
+
+                // Add attributes using DocumentAttributes class
+                foreach ($data['attributes']->getAttributes() as $attrName => $attrValue) {
+                    $presentation->addAttribute($attrName, $attrValue);
                 }
             }
             $result = $xml->asXML();
