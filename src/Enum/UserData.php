4cbf5e62d0db049204ffead00dcd8e2468b3a1e5
Ghazi Triki <ghazi.triki@riadvice.tn>
2025-04-03 19:27:51 +0100

    Add new userdata values

--- src/Enum/UserData.php
+++ src/Enum/UserData.php
@@ -23,17 +23,20 @@ namespace BigBlueButton\Enum;
 enum UserData: string
 {
     // Application settings
-    case ASK_FOR_FEEDBACK_ON_LOGOUT     = 'bbb_ask_for_feedback_on_logout';
-    case AUTO_JOIN_AUDIO                = 'bbb_auto_join_audio';
-    case CLIENT_TITLE                   = 'bbb_client_title';
-    case FORCE_LISTEN_ONLY              = 'bbb_force_listen_only';
-    case LISTEN_ONLY_MODE               = 'bbb_listen_only_mode';
-    case SKIP_CHECK_AUDIO               = 'bbb_skip_check_audio';
-    case SKIP_CHECK_AUDIO_ON_FIRST_JOIN = 'bbb_skip_check_audio_on_first_join';
-    case OVERRIDE_DEFAULT_LOCALE        = 'bbb_override_default_locale';
-    case HIDE_PRESENTATION_ON_JOIN      = 'bbb_hide_presentation_on_join';
-    case DIRECT_LEAVE_BUTTON            = 'bbb_direct_leave_button';
-
+    case ASK_FOR_FEEDBACK_ON_LOGOUT       = 'bbb_ask_for_feedback_on_logout';
+    case AUTO_JOIN_AUDIO                  = 'bbb_auto_join_audio';
+    case CLIENT_TITLE                     = 'bbb_client_title';
+    case FORCE_LISTEN_ONLY                = 'bbb_force_listen_only';
+    case LISTEN_ONLY_MODE                 = 'bbb_listen_only_mode';
+    case SKIP_CHECK_AUDIO                 = 'bbb_skip_check_audio';
+    case SKIP_CHECK_AUDIO_ON_FIRST_JOIN   = 'bbb_skip_check_audio_on_first_join';
+    case SKIP_ECHOTEST_IF_PREVIOUS_DEVICE = 'bbb_skip_echotest_if_previous_device';
+    case OVERRIDE_DEFAULT_LOCALE          = 'bbb_override_default_locale';
+    case DIRECT_LEAVE_BUTTON              = 'bbb_direct_leave_button';
+    case PARENT_ROOM_MODERATOR            = 'bbb_parent_room_moderator';
+    case RECORD_PERMISSION                = 'bbb_record_permission';
+    case RECORD_PERMISSION_TOOLTIP        = 'bbb_record_permission_tooltip';
+    case SHOW_SESSION_DETAILS_ON_JOIN     = 'bbb_show_session_details_on_join';
     // Branding settings
     case DISPLAY_BRANDING_AREA = 'bbb_display_branding_area';
 
@@ -41,15 +44,16 @@ enum UserData: string
     case SHORTCUTS = 'bbb_shortcuts';
 
     // Kurento settings (WebRTC media server)
-    case AUTO_SHARE_WEBCAM                = 'bbb_auto_share_webcam';
-    case PREFERRED_CAMERA_PROFILE         = 'bbb_preferred_camera_profile';
-    case ENABLE_VIDEO                     = 'bbb_enable_video';
-    case RECORD_VIDEO                     = 'bbb_record_video';
-    case SKIP_VIDEO_PREVIEW               = 'bbb_skip_video_preview';
-    case SKIP_VIDEO_PREVIEW_ON_FIRST_JOIN = 'bbb_skip_video_preview_on_first_join';
-    case MIRROR_OWN_WEBCAM                = 'bbb_mirror_own_webcam';
-    case FULLAUDIO_BRIDGE                 = 'bbb_fullaudio_bridge';
-    case TRANSPARENT_LISTEN_ONLY          = 'bbb_transparent_listen_only';
+    case AUTO_SHARE_WEBCAM                     = 'bbb_auto_share_webcam';
+    case PREFERRED_CAMERA_PROFILE              = 'bbb_preferred_camera_profile';
+    case ENABLE_VIDEO                          = 'bbb_enable_video';
+    case RECORD_VIDEO                          = 'bbb_record_video';
+    case SKIP_VIDEO_PREVIEW                    = 'bbb_skip_video_preview';
+    case SKIP_VIDEO_PREVIEW_ON_FIRST_JOIN      = 'bbb_skip_video_preview_on_first_join';
+    case SKIP_VIDEO_PREVIEW_IF_PREVIOUS_DEVICE = 'bbb_skip_video_preview_if_previous_device';
+    case MIRROR_OWN_WEBCAM                     = 'bbb_mirror_own_webcam';
+    case FULLAUDIO_BRIDGE                      = 'bbb_fullaudio_bridge';
+    case TRANSPARENT_LISTEN_ONLY               = 'bbb_transparent_listen_only';
 
     // Presentation settings
     case FORCE_RESTORE_PRESENTATION_ON_NEW_EVENTS = 'bbb_force_restore_presentation_on_new_events';
@@ -60,15 +64,19 @@ enum UserData: string
     case MULTI_USER_TOOLS    = 'bbb_multi_user_tools';
 
     // Theming & Styling settings
-    case CUSTOM_STYLE     = 'bbb_custom_style';
-    case CUSTOM_STYLE_URL = 'bbb_custom_style_url';
+    case CUSTOM_STYLE      = 'bbb_custom_style';
+    case CUSTOM_STYLE_URL  = 'bbb_custom_style_url';
+    case PREFER_DARK_THEME = 'bbb_prefer_dark_theme';
 
     // Layout settings
     case AUTO_SWAP_LAYOUT           = 'bbb_auto_swap_layout';
     case HIDE_PRESENTATION          = 'bbb_hide_presentation';
+    case HIDE_PRESENTATION_ON_JOIN  = 'bbb_hide_presentation_on_join';
     case SHOW_PARTICIPANTS_ON_LOGIN = 'bbb_show_participants_on_login';
     case SHOW_PUBLIC_CHAT_ON_LOGIN  = 'bbb_show_public_chat_on_login';
     case HIDE_NAV_BAR               = 'bbb_hide_nav_bar';
     case HIDE_ACTIONS_BAR           = 'bbb_hide_actions_bar';
     case DEFAULT_LAYOUT             = 'bbb_default_layout';
+    case HIDE_NOTIFICATIONS         = 'bbb_hide_notifications';
+    case HIDE_CONTROLS              = 'bbb_hide_controls';
 }
