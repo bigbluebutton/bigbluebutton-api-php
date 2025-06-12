20152b793d75805c012a4a5b3c8c6f3d7651d19e
Andreas Hennings <andreas@dqxtech.net>
2025-06-12 14:59:15 +0200

    Also check for ->getDisabledFeaturesExclude() in BigBlueButtonTest::testInsertDocumentFile().

--- tests/BigBlueButtonTest.php
+++ tests/BigBlueButtonTest.php
@@ -309,7 +309,9 @@ class BigBlueButtonTest extends TestCase
         $insertDocumentResponse = $this->bbb->insertDocument($insertDocumentParameters);
 
         // ASSERT
-        if (!in_array(Feature::PRESENTATION, $createMeetingParameters->getDisabledFeatures()) {
+        if (!in_array(Feature::PRESENTATION, $createMeetingParameters->getDisabledFeatures())
+            || in_array(Feature::PRESENTATION, $createMeetingParameters->getDisabledFeaturesExclude())
+        ) {
             $this->assertTrue($insertDocumentResponse->success());
         } else {
             $this->assertTrue($insertDocumentResponse->failed());
